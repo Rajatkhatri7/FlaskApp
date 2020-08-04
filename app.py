b8a5581f4da70c2aed10c948c5ba2cc6e9d08548
rajat <rajatkhatri0002@gmail.com>
2020-08-05 00:14:34 +0530

    home page route

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask
 #flask obj
 app = Flask(__name__)
 
+
+@app.route("/")
+def index():
+    return "Home Page" #can add html inside the quotes
+
+
 #decorater route to the main page
 @app.route('/home/users/<string:name>/posts/<int:id>') #used in dynamic url like name keep changing  etc
 def hello(name,id):

0773ace268c9e1a27a4c87acb43ce73f3fa54878
rajat <rajatkhatri0002@gmail.com>
2020-08-05 00:09:08 +0530

    get and post methods

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask import Flask
 app = Flask(__name__)
 
 #decorater route to the main page
-@app.route('/home/<string:name>') #used in dynamic url like name keep changing  etc
-def hello(name):
-    return "Hello "+name
+@app.route('/home/users/<string:name>/posts/<int:id>') #used in dynamic url like name keep changing  etc
+def hello(name,id):
+    return "Hello "+name+" your id is "+ str(id)
+# can add images if we render images on every refresh
+
+
+#page to allow get req
+@app.route("/getpage", methods = ['GET']) # POST for post method 
+def get_req():
+    return "only getthis web page."
 
 
 if __name__ == '__main__':

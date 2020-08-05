3a53987ad93392fe60b947b5cd3eea23f7ce4e56
rajat <rajatkhatri0002@gmail.com>
2020-08-06 04:17:33 +0530

    seprate page for creating posts

--- app.py
+++ app.py
@@ -21,6 +21,66 @@ class BlogPost(db.Model):
     author = db.Column(db.String(20), nullable=False, default='N/A')
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    
+    post = BlogPost.query.get_or_404(id)
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.author = request.form['author']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+def new_post():
+
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')
+
+
 #commands for python shell to connect darabase
 #from app import db
 #db.create_all()
@@ -35,9 +95,7 @@ class BlogPost(db.Model):
 # >>> db.session.delete( BlogPost.query.get(3)) >>> db.session.commit() Delete a post
 # >>> BlogPost.query.get(1).title = "New POSSSSSr"  >>> db.session.commit()  Change the variables
     # represent the database
-    def __repr__(self):
-        return "Blog Post "+str(self.id)
-
+    
 #dummy data
 
 all_posts = [
@@ -63,47 +121,9 @@ all_posts = [
 
 
 
-@app.route("/")
-def index():
-    return render_template('index.html')
 
-@app.route("/posts",methods = ['GET','POST'])
-def posts():
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        post_author = request.form['author']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
-        return render_template('post.html',posts = all_posts)
-
-
-#deleting a post
-@app.route('/posts/delete/<int:id>')
-def delete(id):
-    post = BlogPost.query.get_or_404(id)
-    db.session.delete(post)
-    db.session.commit()
-    return redirect('/posts')
-
-
-#edit the post 
-@app.route('/posts/edit/<int:id>',methods = ['GET','POST'])
-def edit(id):
-    post = BlogPost.query.get_or_404(id)
-
-    if request.method == 'POST':
-        post.title = request.form['title']
-        post.author = request.form['author']
-        post.content = request.form['content']
-        db.session.commit()
-        return redirect('/posts')   
-    else:
-        return render_template('edit.html',post=post)   
+    
+     
 
 #decorater route to the main page
 @app.route('/home/users/<string:name>/posts/<int:id>') #used in dynamic url like name keep changing  etc
